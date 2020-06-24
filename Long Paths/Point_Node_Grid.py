92aa3353e03d71f6ec3dfbb9cac4cadaad26d6c2
petiesmo <66927015+petiesmo@users.noreply.github.com>
2020-06-24 17:41:45 -0600

    Simplifying to short paths

--- Long Paths/Point_Node_Grid.py
+++ Long Paths/Point_Node_Grid.py
@@ -1,47 +1,51 @@
 ''' Point_Node_Grid.py
 Defines classes for grid-like data structures, images, nodes and paths
 Default orientation: +X right, +Y down  (consistent with Col/Row and Numpy arrays)
-Assumption: Each point along path is a Node, with path (length = 1) to neighbors
+LONG PATHS: Perspective is that nodes only exist at intersections, with paths inbetween
+(vice SHORT PATHS: In a grid, each point is a node with a short path to neighbors - may be slower at large grid sizes)
 '''
 
 import logging
 import itertools as IT
-from pprint import pprint
-
-def get_nbrs(row, col):
-    '''Returns a tuple of adjacent cell coordinates (Up,Right,Down,Left)
-        Assumes (x right, y down)'''  
-    #tpadd = lambda a, b: tuple(m+n for m, n in zip(a, b))
-    delr = (-1, 0, 1, 0)
-    delc = (0, 1, 0, -1)
-    nbrs = [tpadd((row, col), z) for z in zip(delr, delc)]
-    return tuple(nbrs)  #(up,right,down,left) 
-
-def tpadd(a, b):
-    return tuple(m+n for m, n in zip(a, b))
-
 
 class Point():  
     '''Defines a generic Point data structure to track Location and State information'''
-    def __init__(self, x, y, state=False, **kwargs):
+    def __init__(self, x, y, state='-'):
         self.x = x
         self.y = y
         self.state = state
-        self.__dict__.update(kwargs)
+        self.is_node = False
+        self.is_path = False
+        self.nbrs = get_nbrs(y,x)
     
     def __repr__(self):
-        return f'Point(x={self.x}, y={self.y}, state={self.state})'
+        return f'Point(x={self.x}, y={self.y}, state={self.state}, node? {self.is_node})'
+
+    @staticmethod
+    def get_nbrs(row, col):
+            '''Returns a tuple of adjacent cell coordinates (Up,Right,Down,Left)
+                Assumes (x right, y down)'''  
+            #tpadd = lambda a, b: tuple(m+n for m, n in zip(a, b))
+            delr = (-1, 0, 1, 0)
+            delc = (0, 1, 0, -1)
+            nbrs = [tpadd((row, col), z) for z in zip(delr, delc)]
+            return tuple(nbrs)  #(up,right,down,left)
+    
+    @staticmethod
+    def tpadd(a, b):
+        return tuple(m+n for m, n in zip(a, b))
         
     #End class Point
 
-
 class Node(Point):
     '''Special type of Point, with linkage to other nodes in a network'''
-    def __init__(self, node_id, x, y, state=False):
-        super().__init__(x, y, state=state, node_id=node_id, nbrs=[], paths=[])
+    def __init__(self, node_id, x, y):
+        self.node_id = node_id
+        super().__init__(x, y, 'O')
+        self.paths = []
     
     def __repr__(self):
-        return f'Node(id:{self.node_id},x:{self.x}, y:{self.y})'
+        return f'Node(id:{self.node_id},x:{self.x}, y:{self.y}, paths:{len(self.paths)})'
 
     #End class Node
 
@@ -50,21 +54,18 @@ class Grid():
     Adjusts Row-Column window to fit x,y extents of Points'''
     
     def __init__(self, points, originxy=(0,0)):
-        self.points = points           #Object list (can be sparse)
+        self.points = points            #Object list (can be sparse)
         self.originxy = originxy       #Maintains translation from row,col to x,y
-        self.nodes = [originxy]
+        self.nodes = []
         self.paths = []
     
     @property
     def array(self):
-        self.find_extents()
-        array = [['NaN' for col in range(self.ncols)] for row in range(self.nrows)]
-        for p in self.points:
-            r,c = self.convert_to_rc(p.x,p.y)
-            array[r][c] = p
-        return array
-        
-    def find_extents(self):
+        a,b,c,d = extents(self)
+        return [[0 for p in range(self.ncols)] for row in range(self.nrows)]
+    
+    @property    
+    def extents(self):
         ''' Returns a tuple of x,y coordinates defining corners of a bounding box rectangle'''
         self.xmin = min([p.x for p in self.points])
         self.xmax = max([p.x for p in self.points])
@@ -72,33 +73,37 @@ class Grid():
         self.ymin = min([p.y for p in self.points])
         self.ymax = max([p.y for p in self.points])
         self.nrows = self.ymax - self.ymin + 1
-        self.originrc = tpadd((-self.xmin,-self.ymin), self.originxy)
         return None 
+    
+    @property
+    def originrc(self):
+        originrc = Point.tpadd((-self.xmin,-self.ymin),self.originxy)
+        return self.originrc
         
     def convert_to_rc(self, x, y):
-        r,c = self.originrc
-        r = y - self.ymin
-        c = x - self.xmin
+        r = self.originrc + y
+        c = self.originrc + x
         return (r,c)
     
-    def pad_grid(self,negx,posx,negy,posy):
-        ''' Future implementation'''
+    def pad_grid(self,_x,x,_y,y):
+        self.array
         return None
     
     def __repr__(self):
-        return f'Grid(rows:{self.nrows}, cols:{self.ncols}, origin:{self.originrc})'
+        return f'Grid(rows:{self.rows}, cols:{self.cols}, origin:{self.origin})'
     
     def find_nodes(self):
         '''Processing script: accepts an array (list of lists) of points, 
         Checks each point against node criteria
         Modifies Point object is_node value
-        Returns a list of Node objects'''
-        in_grid = lambda r,c: (r in range(self.nrows)) and (c in range(self.ncols)) 
+        Returns a list of Node objects 
+        (Here, Node Criteria implies CORNERS, CROSSES, DEAD ENDS only)'''
+        in_grid = lambda r,c: (r in range(self.rows)) and (c in range(self.cols)) 
         points = [p for p in IT.chain.from_iterable(self)]
         for p in points:
             nbr_vals = [self.array[nr][nc].state if in_grid(nr,nc) else 'NaN' for (nr,nc) in p.nbrs]
             if p.is_scaff:
-                #p.is_path = nbr_vals[0] == nbr_vals[2] and nbr_vals[1] == nbr_vals[3] and nbr_vals[0] != nbr_vals[1]
+                p.is_path = (nbr_vals[0] == nbr_vals[2] = '#') ^ (nbr_vals[1] == nbr_vals[3] == '#') 
                 p.is_node = not(is_path)
         ids = IT.count(0)
         return [Node(next(ids), p.x, p.y) for p in points if p.is_node]
@@ -115,10 +120,10 @@ class Grid():
             node_row = [n for n in nodes if n.row == i]
             for n in range(1, len(node_row)):
                 nodeL, nodeR = node_row[n-1], node_row[n]
-                span = point_row[nodeL.col+1 : nodeR.col]
+                span = point_row[nodeL.col : 1+nodeR.col]
                 if all([p.is_scaff for p in span]):
                     link = next(links)
-                    paths[link] = ('H', len(span), nodeL.node_id, nodeR.node_id)
+                    paths[link] = ('H', (nodeR.row - nodeL.row), nodeL.node_id, nodeR.node_id)
                     nodeL.paths.append((link, len(span), nodeR.node_id))
                     nodeR.paths.append((link, len(span), nodeL.node_id))
 
@@ -128,22 +133,12 @@ class Grid():
             node_col = [n for n in nodes if n.col == i]        
             for n in range(1, len(node_col)):
                 nodeL, nodeR = node_col[n-1], node_col[n]
-                span = point_col[nodeL.col+1 : nodeR.col]
+                span = point_col[nodeL.row : 1+nodeR.row]
                 if all([p.is_scaff for p in span]):
                     link = next(links)
-                    paths[link] = ('V', len(span), nodeL.node_id, nodeR.node_id)
-                    nodeL.paths.append((link, len(span), nodeR.node_id))
-                    nodeR.paths.append((link, len(span), nodeL.node_id))
+                    link_length = (nodeR.col - nodeL.col)
+                    paths[link] = ('V', link_length , nodeL.node_id, nodeR.node_id)
+                    nodeL.paths.append((link, link_length, nodeR.node_id))
+                    nodeR.paths.append((link, link_length, nodeL.node_id))
         return tuple(nodes), paths
     #End class Grid
-
-
-def test():
-    points = [Point(x,y,'.') for y in range(0,6,2) for x in range(0,8,2)]
-    pprint(points)
-
-    grid = Grid(points)
-    pprint(grid.array)
-
-if __name__ == "__main__":
-    test()    
