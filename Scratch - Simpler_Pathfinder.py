b51b55df6a190c5e46825541073dcf93fed9876c
petiesmo <66927015+petiesmo@users.noreply.github.com>
2020-06-30 19:28:03 -0600

    Still deugging, getting closer

--- Scratch - Simpler_Pathfinder.py
+++ Scratch - Simpler_Pathfinder.py
@@ -40,41 +40,62 @@ class Node(Point):
     #End class Node
 
 
-def check_path(grid, current_node, parent, this_path, destination):
+def check_path(grid, current_node, parent, destination):
     cx, cy = current_node.x, current_node.y
-    path.append(current_node)
-	if current_node == destination:
+      
+    if current_node == destination:
+        test_path.append(current_node)
+        valid_paths.append(test_path)
         return True
-    
     valid_nbrs = {nbr for nbr in [grid[ny][nx] for (ny,nx) in get_nbrs(cy,cx)] 
-                    if type(nbr) is Node and nbr in candidates} - {parent}
+                    if type(nbr) is Node} - set(test_path)
     print(f'Cnode {current_node}, Nb: {valid_nbrs}')
     if not valid_nbrs:
         candidates.remove(current_node)
-        return None
-    else:
-       
-        for nbr in valid_nbrs:				
-            current_path = check_path(grid, nbr, current_node, this_path, destination)		#Recursion
-            if current_path:
-                    if destination in current_path:
-                        paths.append(current_path)    
-            best_path = min(paths, default=[], key=lambda x: len(x))
-            
-        return best_path
+        return False    # Dead End
+    test_path.append(current_node)
+    for nbr in valid_nbrs:				
+        if check_path(grid, nbr, current_node, destination):    #Recursion
+            break		
+    candidates.remove(current_node)
+    return None
 
+def get_image(stream):
+    ''' Runs the input software into the Intcode computer, reads output stream,
+    and stores data for each new Pixel object
+    Returns an array (tuple of tuples) of Pixel objects'''
+    screen = stream.rstrip('\n\r').splitlines()
+    pixels = [[Point(jcol,irow,p) 
+           for jcol,p in enumerate(row)] 
+           for irow,row in enumerate(screen)]
+    #logging.debug(pixels[:][:])
+    return tuple((tuple(line) for line in pixels))
 
-grid = [['.' for p in range(6)] for r in range(6)]
-nodes = [(0,0), (0,1), (0,2), (1,1), (1,2), (2,2), (3,2), (3,3), (2,4), (3,4), (4,4)]
-nids = IT.count(0)
-for node in nodes:
-    nx, ny = node
-    grid[ny][nx] = Node(nx, ny, next(nids))
-candidates = [spot for spot in [grid[ny][nx] for nx in range(6) for ny in range(6)] if type(spot) is Node]
-all_paths = []
+test_grid ='''........
+.XXXXXX.
+.X....X.
+.XXXXXX.
+.X..X...
+..XXX...
+....XXX.
+........'''
+
+#grid = [['.' for p in range(6)] for r in range(6)]
+#nodes = [(1,1), (1,2), (1,3), (1,4), (2,3), (3,3), (3,4), (4,4), ()]
+
+grid = get_image(test_grid)
 from pprint import pprint
 pprint(grid)
+nids = IT.count(0)
+nodes = [[Node(p.x, p.y, next(nids)) if p.state == 'X' else p for p in row ] for row in grid]
+pprint(nodes)
+candidates = set([n for n in IT.chain.from_iterable(nodes) if type(n) is Node])
 
-best_path = check_path(grid, candidates, grid[0][0], '_noparent', [], grid[4][4])
+valid_paths = []
+while candidates:    
+    test_path = []
+    check_path(grid, nodes[1][1], '_noparent', nodes[6][6])
+ 
+best_path = min(valid_paths, key=len, default='No valid paths')
 print(best_path)
 
